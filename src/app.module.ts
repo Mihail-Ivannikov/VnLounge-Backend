0825470d1529f1710f5b70d0d66e1e492a6d0114
Mihail-Ivannikov <ivannikovmihail96@gmail.com>
2024-12-27 01:37:43 +0200

    works visual novel display (all or one), torrent file download

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { VisualNovelsModule } from './visual-novels/visual-novels.module';
 import { ConfigModule } from '@nestjs/config';
 import { VisualNovel } from './visual-novels/visual-novel.entity';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -17,6 +19,11 @@ import { VisualNovel } from './visual-novels/visual-novel.entity';
       entities: [VisualNovel],
     }),
     VisualNovelsModule,
+    // ServeStaticModule to serve static files (like images)
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'), // Path to your 'upload' directory
+      serveRoot: '/uploads', // URL path prefix for the images (e.g., http://localhost:8000/uploads)
+    }),
   ],
 })
 export class AppModule {}
