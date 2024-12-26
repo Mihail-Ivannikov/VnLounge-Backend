d26c6a8583bfebd3b29aa2f8bae955491a3ac0db
Mihail-Ivannikov <ivannikovmihail96@gmail.com>
2024-12-27 01:38:40 +0200

    torrent download and visual novels display works

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,25 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier/@typescript-eslint', // Ensure Prettier works well with TypeScript
   ],
   root: true,
   env: {
     node: true,
     jest: true,
+    es2021: true, // You can specify the latest ECMAScript version here
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off', // You can remove this if you don't need it
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error', // This will throw errors for code not following Prettier rules
+    'no-console': 'warn', // You can tweak rules based on your preference
   },
 };
